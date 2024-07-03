85d55e4ddec441d3fc07e915eae9ee7ed6c1c368
Venkatamaruti P <venkie568@gmail.com>
2024-07-03 17:18:12 +0530

    feat: add tasks and remove tasks

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import{useState} from 'react'
 function App() {
+  const [todos, setTodos] =useState([])
+  const [task, setTask] = useState('')
+  const [checkedTasks, setCheckedTasks] = useState([])
+
+  const addTask =()=> {
+    setTodos([...todos, task]);
+    setTask('');
+  }
+  const handleCheck = (e)=> {
+    const { value, checked } = e.target;
+    if(checked)
+      {
+        setCheckedTasks([...checkedTasks, value])
+
+      }
+      else {
+        checkedTasks.forEach((item, index)=>{
+          if(item===value) {
+            checkedTasks.splice(index,1)
+          }
+        })
+        setCheckedTasks([...checkedTasks])
+
+      }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick ={addTask} > Add Task</button>
+      {todos?.map((todo , index)=> {
+        return <h1 key={index}>
+          <input type="checkbox" value={todo} onChange={handleCheck}/>
+          {todo}
+        </h1>
+      })}
+      {checkedTasks?.map((task)=>
+        <li>
+          {task}
+          </li>)}
     </div>
   );
 }
